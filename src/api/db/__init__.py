b3aac7427f7ed81d1b495b6a7649535bc62dfaf8
JCantu248 <jcantu248@gmail.com>
2022-03-14 10:23:21 -0500

    Removing traces of PyMODM from Li-PCA. Reverting to a straight Pymongo in a manner mirroring Con-PCA

--- src/api/db/__init__.py
+++
@@ -1,9 +0,0 @@
-"""The database library."""
-# cisagov Libraries
-from api.connect import (
-    connect_from_config,
-    db_from_config,
-    get_connection_db_by_mongoclient,
-)
-
-__all__ = ["connect_from_config", "get_connection_db_by_mongoclient", "db_from_config"]
