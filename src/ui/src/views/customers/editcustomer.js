a3073ecca97cc6b347067edbd1fcb2a1ec02f74b
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-03-23 17:45:32 -0400

    Update to functional components with hooks
    One hook will toggle the add customer contact card and the other will pass data along to the forms and back

--- src/ui/src/views/customers/editcustomer.js
+++ src/ui/src/views/customers/editcustomer.js
@@ -159,119 +159,122 @@ const cusCampaignsCols = [
 ];
 const cusCampaignsData = { rows: cusCampaignsRows, columns: cusCampaignsCols };
 
-class EditCustomerPage extends React.Component {
-  constructor() {
-    super();
-    this.state = { isToggleOn: true };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState((state) => ({ isToggleOn: !state.isToggleOn }));
-  }
-  render() {
-    return (
-      <MainCard title="Campaigns">
-        <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1000 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <Typography variant="h4" gutterBottom component="div">
-                Customer Information
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <CustomerForm
-                cusContactsData={cusContactsData}
-                cusCampaignsData={cusCampaignsData}
-              ></CustomerForm>
-            </Grid>
-            <Grid item xs={10} sm={6} md={5} lg={4} xl={3} sx={{ mt: 3 }}>
-              <Button
-                color="warning"
-                variant="contained"
-                size="large"
-                fullWidth
-                onClick={this.handleClick}
-              >
-                Add Customer Contact
-              </Button>
-            </Grid>
-            <Grid
-              item
-              display={{ xs: "none", sm: "block" }}
-              sm={3}
-              md={5}
-              lg={6}
-              xl={8}
-            ></Grid>
-            <Grid
-              item
-              display={{ xs: "none", sm: "block" }}
-              sm={3}
-              md={2}
-              lg={2}
-              xl={1}
-              sx={{ mt: 2 }}
-            >
-              <Button size="large" fullWidth>
-                Clear
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mt: 3 }}>
-              <Typography variant="h4" gutterBottom component="div">
-                Customer/Organization Point of Contacts
-              </Typography>
-            </Grid>
-            {this.state.isToggleOn ? (
-              ""
-            ) : (
-              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Card variant="outlined">
-                  <CustomerPOCForm />
-                </Card>
-              </Grid>
-            )}
-            <Grid
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-              xl={12}
-              sx={{ mt: 2, mb: 1 }}
-            >
-              <DisplayDataTable data={cusContactsData} />
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mt: 2 }}>
-              <Typography variant="h4" gutterBottom component="div">
-                Customer Campaigns
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 3 }}>
-              <DisplayDataTable data={cusCampaignsData} />
-            </Grid>
-            <Grid
-              item
-              display={{ xs: "none", sm: "block" }}
-              sm={5}
-              md={7}
-              lg={8}
-              xl={8}
-            ></Grid>
-            <Grid item xs={10} sm={5} md={4} lg={3} xl={3}>
-              <Button disabled variant="contained" size="large" fullWidth>
-                Save Customer
-              </Button>
-            </Grid>
-            <Grid item xs={10} sm={1} md={1} lg={1} xl={1}>
-              <Button color="dark" variant="text" size="large" fullWidth>
-                Cancel
-              </Button>
-            </Grid>
+const EditCustomerPage = () => {
+  const [isToggleOn, setToggle] = React.useState(true);
+  const [custForm, setCustform] = React.useState({
+    custName: "",
+    custId: "",
+    custDomain: "",
+    dateVal: Date.now(), // TODO: change date type follow back-end
+    custType: "",
+    addressOne: "",
+    addressTwo: "",
+    city: "",
+    state: "",
+    zip: "",
+    contact_list: {
+      firstName: "",
+      lastName: "",
+      title: "",
+      officePhone: "",
+      mobilePhone: "",
+      email: "",
+      contactNotes: "",
+    },
+  });
+  console.log(custForm);
+  return (
+    <MainCard title="Campaigns">
+      <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1000 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Typography variant="h4" gutterBottom component="div">
+              Customer Information
+            </Typography>
           </Grid>
-        </Box>
-      </MainCard>
-    );
-  }
-}
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <CustomerForm setCustform={setCustform} custForm={custForm} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 1 }}>
+            <Typography variant="h4" gutterBottom component="div">
+              Customer/Organization Point of Contacts
+            </Typography>
+          </Grid>
+          {isToggleOn ? (
+            <React.Fragment>
+              <Grid item xs={10} sm={6} md={5} lg={4} xl={3} sx={{ mb: 1 }}>
+                <Button
+                  color="warning"
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  onClick={() => setToggle(!isToggleOn)}
+                >
+                  Add Customer Contact
+                </Button>
+              </Grid>
+              <Grid
+                item
+                display={{ xs: "none", sm: "block" }}
+                sm={6}
+                md={7}
+                lg={8}
+                xl={9}
+              ></Grid>
+            </React.Fragment>
+          ) : (
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 1 }}>
+              <Card variant="outlined">
+                <CustomerPOCForm
+                  setCustform={setCustform}
+                  custForm={custForm}
+                  isToggleOn={isToggleOn}
+                  setToggle={setToggle}
+                />
+              </Card>
+            </Grid>
+          )}
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+            sx={{ mt: 2, mb: 1 }}
+          >
+            <DisplayDataTable data={cusContactsData} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mt: 2 }}>
+            <Typography variant="h4" gutterBottom component="div">
+              Customer Campaigns
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 3 }}>
+            <DisplayDataTable data={cusCampaignsData} />
+          </Grid>
+          <Grid
+            item
+            display={{ xs: "none", sm: "block" }}
+            sm={5}
+            md={7}
+            lg={8}
+            xl={8}
+          ></Grid>
+          <Grid item xs={10} sm={5} md={4} lg={3} xl={3}>
+            <Button disabled variant="contained" size="large" fullWidth>
+              Save Customer
+            </Button>
+          </Grid>
+          <Grid item xs={10} sm={1} md={1} lg={1} xl={1}>
+            <Button color="dark" variant="text" size="large" fullWidth>
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </MainCard>
+  );
+};
 
 export default EditCustomerPage;
--- src/ui/src/views/customers/newcustomer.js
+++ src/ui/src/views/customers/newcustomer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 // material-ui
 import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box";
@@ -13,100 +14,113 @@ import MainCard from "ui-component/cards/MainCard";
 
 // ==============================|| New Customer View ||============================== //
 
-class NewCustomerPage extends React.Component {
-  constructor() {
-    super();
-    this.state = { isToggleOn: true };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState((state) => ({ isToggleOn: !state.isToggleOn }));
-  }
-  render() {
-    return (
-      <MainCard title="Campaigns">
-        <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1000 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <Typography variant="h4" gutterBottom component="div">
-                Customer Information
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <CustomerForm />
-            </Grid>
-            <Grid item xs={10} sm={6} md={5} lg={4} xl={3} sx={{ mt: 3 }}>
-              <Button
-                color="warning"
-                variant="contained"
-                size="large"
-                fullWidth
-                onClick={this.handleClick}
-              >
-                Add Customer Contact
-              </Button>
-            </Grid>
-            <Grid
-              item
-              display={{ xs: "none", sm: "block" }}
-              sm={3}
-              md={5}
-              lg={6}
-              xl={8}
-            ></Grid>
-            <Grid
-              item
-              display={{ xs: "none", sm: "block" }}
-              sm={3}
-              md={2}
-              lg={2}
-              xl={1}
-              sx={{ mt: 2 }}
-            >
-              <Button size="large" fullWidth>
-                Clear
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mt: 3 }}>
-              <Typography variant="h4" gutterBottom component="div">
-                Customer/Organization Point of Contacts
-              </Typography>
-            </Grid>
-            {this.state.isToggleOn ? (
-              ""
-            ) : (
-              <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 1 }}>
-                <Card variant="outlined">
-                  <CustomerPOCForm />
-                </Card>
-              </Grid>
-            )}
-            <Grid item xs={10} sm={12} md={12} lg={12} xl={12} sx={{ mb: 3 }}>
-              <Alert severity="error">No contacts available</Alert>
-            </Grid>
-            <Grid
-              item
-              display={{ xs: "none", sm: "block" }}
-              sm={5}
-              md={7}
-              lg={8}
-              xl={8}
-            ></Grid>
-            <Grid item xs={10} sm={5} md={4} lg={3} xl={3}>
-              <Button disabled variant="contained" size="large" fullWidth>
-                Save Customer
-              </Button>
-            </Grid>
-            <Grid item xs={10} sm={1} md={1} lg={1} xl={1}>
-              <Button color="dark" variant="text" size="large" fullWidth>
-                Cancel
-              </Button>
-            </Grid>
+const NewCustomerPage = () => {
+  let navigate = useNavigate();
+  const [isToggleOn, setToggle] = React.useState(true);
+  const [custForm, setCustform] = React.useState({
+    custName: "",
+    custId: "",
+    custDomain: "",
+    dateVal: Date.now(), // TODO: change date type follow back-end
+    custType: "",
+    addressOne: "",
+    addressTwo: "",
+    city: "",
+    state: "",
+    zip: "",
+    contact_list: {
+      firstName: "",
+      lastName: "",
+      title: "",
+      officePhone: "",
+      mobilePhone: "",
+      email: "",
+      contactNotes: "",
+    },
+  });
+  console.log(custForm);
+  return (
+    <MainCard title="Campaigns">
+      <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1000 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Typography variant="h4" gutterBottom component="div">
+              Customer Information
+            </Typography>
           </Grid>
-        </Box>
-      </MainCard>
-    );
-  }
-}
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <CustomerForm setCustform={setCustform} custForm={custForm} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 1 }}>
+            <Typography variant="h4" gutterBottom component="div">
+              Customer/Organization Point of Contacts
+            </Typography>
+          </Grid>
+          {isToggleOn ? (
+            <React.Fragment>
+              <Grid item xs={10} sm={6} md={5} lg={4} xl={3} sx={{ mb: 1 }}>
+                <Button
+                  color="warning"
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  onClick={() => setToggle(!isToggleOn)}
+                >
+                  Add Customer Contact
+                </Button>
+              </Grid>
+              <Grid
+                item
+                display={{ xs: "none", sm: "block" }}
+                sm={6}
+                md={7}
+                lg={8}
+                xl={9}
+              ></Grid>
+            </React.Fragment>
+          ) : (
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ mb: 1 }}>
+              <Card variant="outlined">
+                <CustomerPOCForm
+                  setCustform={setCustform}
+                  custForm={custForm}
+                  isToggleOn={isToggleOn}
+                  setToggle={setToggle}
+                />
+              </Card>
+            </Grid>
+          )}
+          <Grid item xs={10} sm={12} md={12} lg={12} xl={12} sx={{ mb: 3 }}>
+            <Alert severity="error">No contacts available</Alert>
+          </Grid>
+          <Grid
+            item
+            display={{ xs: "none", sm: "block" }}
+            sm={5}
+            md={7}
+            lg={8}
+            xl={8}
+          ></Grid>
+          <Grid item xs={10} sm={5} md={4} lg={3} xl={3}>
+            <Button disabled variant="contained" size="large" fullWidth>
+              Save Customer
+            </Button>
+          </Grid>
+          <Grid item xs={10} sm={1} md={1} lg={1} xl={1}>
+            <Button
+              color="dark"
+              variant="text"
+              size="large"
+              fullWidth
+              onClick={() => navigate("/li-pca-app/customers")}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </MainCard>
+  );
+};
 
 export default NewCustomerPage;
