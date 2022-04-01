8d90b5a486e6de8e389f60f59f47e7babbf3df85
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-04-01 17:31:27 -0400

    Update CustomerPOCForm to use Formik and Yup

--- src/ui/src/ui-component/forms/CustomerPOCForm.js
+++ src/ui/src/ui-component/forms/CustomerPOCForm.js
@@ -1,49 +1,85 @@
+import PropTypes from "prop-types";
+
 // material-ui
 import AddIcon from "@mui/icons-material/Add";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import FormControl from "@mui/material/FormControl";
+import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 
-export default function CustomerPOCForm(props) {
+//third party
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  first_name: yup.string().required("First Name is required"),
+  last_name: yup.string().required("Last Name is required"),
+  email: yup.string().required("Email is required").email("Email is invalid"),
+});
+
+function CustomerPOCForm(props) {
+  const formik = useFormik({
+    initialValues: props.initialPOCValues,
+    validationSchema: validationSchema,
+    onSubmit: (values, actions) => {
+      const cF = [...props.custPOCData];
+      const nextIndex = cF.length;
+      cF[nextIndex] = {
+        id: nextIndex,
+        email: values.email,
+        first_name: values.first_name,
+        last_name: values.last_name,
+        mobile_phone: values.mobile_phone,
+        notes: values.notes,
+        office_phone: values.office_phone,
+        title: values.title,
+      };
+      props.setCustPOCData(cF);
+      props.setHasContact(true);
+      actions.resetForm();
+      props.setToggleCard(!props.isToggleCardOn);
+    },
+  });
   return (
-    // <form onSubmit={formik.handleSubmit}>
-    <div>
+    <Card variant="outlined">
       <CardContent>
-        <FormControl>
+        <form onSubmit={formik.handleSubmit}>
           <Box sx={{ ml: 1 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12} />
               <Grid item xs={10} sm={6} md={6} lg={6} xl={6}>
                 <TextField
                   fullWidth
-                  required
-                  id="firstName"
-                  name="firstName"
-                  label="First Name"
-                  value={props.custForm.contact_list.firstName}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.firstName = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  id="first_name"
+                  name="first_name"
+                  label="First Name *"
+                  value={formik.values.first_name}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.first_name &&
+                    Boolean(formik.errors.first_name)
+                  }
+                  helperText={
+                    formik.touched.first_name && formik.errors.first_name
+                  }
                 />
               </Grid>
               <Grid item xs={10} sm={6} md={6} lg={6} xl={6}>
                 <TextField
                   fullWidth
-                  required
-                  id="lastName"
-                  name="lastName"
-                  label="Last Name"
-                  value={props.custForm.contact_list.lastName}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.lastName = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  id="last_name"
+                  name="last_name"
+                  label="Last Name *"
+                  value={formik.values.last_name}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.last_name && Boolean(formik.errors.last_name)
+                  }
+                  helperText={
+                    formik.touched.last_name && formik.errors.last_name
+                  }
                 />
               </Grid>
               <Grid item xs={10} sm={4} md={4} lg={4} xl={4}>
@@ -52,55 +88,40 @@ export default function CustomerPOCForm(props) {
                   id="title"
                   name="title"
                   label="Title"
-                  value={props.custForm.contact_list.title}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.title = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  value={formik.values.title}
+                  onChange={formik.handleChange}
                 />
               </Grid>
               <Grid item xs={10} sm={4} md={4} lg={4} xl={4}>
                 <TextField
                   fullWidth
-                  id="officePhone"
-                  name="officePhone"
+                  id="office_phone"
+                  name="office_phone"
                   label="Office Phone"
-                  value={props.custForm.contact_list.officePhone}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.officePhone = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  value={formik.values.office_phone}
+                  onChange={formik.handleChange}
                 />
               </Grid>
               <Grid item xs={10} sm={4} md={4} lg={4} xl={4}>
                 <TextField
                   fullWidth
-                  id="mobilePhone"
-                  name="mobilePhone"
+                  id="mobile_phone"
+                  name="mobile_phone"
                   label="Mobile Phone"
-                  value={props.custForm.contact_list.mobilePhone}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.mobilePhone = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  value={formik.values.mobile_phone}
+                  onChange={formik.handleChange}
                 />
               </Grid>
               <Grid item xs={10} sm={10} md={7} lg={7} xl={7}>
                 <TextField
                   fullWidth
-                  required
                   id="email"
                   name="email"
-                  label="Email"
-                  value={props.custForm.contact_list.email}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.email = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  label="Email *"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
                 />
               </Grid>
               <Grid item xs={10} sm={10} md={7} lg={7} xl={7}>
@@ -108,27 +129,23 @@ export default function CustomerPOCForm(props) {
                   fullWidth
                   multiline
                   minRows={2}
-                  id="contactNotes"
-                  name="contactNotes"
+                  id="contact_notes"
+                  name="contact_notes"
                   label="Contact Notes"
-                  value={props.custForm.contact_list.contactNotes}
-                  onChange={(e) => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list.contactNotes = e.target.value;
-                    props.setCustform(cF);
-                  }}
+                  value={formik.values.contact_notes}
+                  onChange={formik.handleChange}
                 />
               </Grid>
               <Grid item xs={2} sm={2} md={5} lg={5} xl={5}></Grid>
               <Grid item xs={10} sm={4} md={3} lg={2} xl={2}>
                 <Button
-                  disabled
                   color="primary"
                   variant="contained"
                   type="submit"
                   size="large"
                   fullWidth
                   endIcon={<AddIcon />}
+                  disabled={!(formik.isValid && formik.dirty)}
                 >
                   Add
                 </Button>
@@ -138,7 +155,7 @@ export default function CustomerPOCForm(props) {
                   color="primary"
                   size="large"
                   fullWidth
-                  onClick={() => props.setToggle(!props.isToggleOn)}
+                  onClick={() => props.setToggleCard(!props.isToggleCardOn)}
                 >
                   Close
                 </Button>
@@ -152,30 +169,25 @@ export default function CustomerPOCForm(props) {
                 lg={2}
                 xl={1}
               >
-                <Button
-                  size="large"
-                  fullWidth
-                  onClick={() => {
-                    const cF = { ...props.custForm };
-                    cF.contact_list = {
-                      firstName: "",
-                      lastName: "",
-                      title: "",
-                      officePhone: "",
-                      mobilePhone: "",
-                      email: "",
-                      contactNotes: "",
-                    };
-                    props.setCustform(cF);
-                  }}
-                >
+                <Button size="large" fullWidth onClick={formik.handleReset}>
                   Reset
                 </Button>
               </Grid>
             </Grid>
           </Box>
-        </FormControl>
+        </form>
       </CardContent>
-    </div>
+    </Card>
   );
 }
+
+CustomerPOCForm.propTypes = {
+  initialPOCValues: PropTypes.object,
+  custPOCData: PropTypes.array,
+  setCustPOCData: PropTypes.func,
+  setHasContact: PropTypes.func,
+  setToggleCard: PropTypes.func,
+  isToggleCardOn: PropTypes.bool,
+};
+
+export default CustomerPOCForm;
