8faaafe4ed9fef2022d498514ef94ce8c08c0f12
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-04-15 10:08:53 -0400

    Add POST call and popup validation

--- src/ui/src/views/customers/newcustomer.js
+++ src/ui/src/views/customers/newcustomer.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 // material-ui
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
@@ -10,6 +15,12 @@ import Typography from "@mui/material/Typography";
 import CustomerForm from "ui-component/forms/CustomerForm";
 import CustomerPOCForm from "ui-component/forms/CustomerPOCForm";
 import MainCard from "ui-component/cards/MainCard";
+
+// third party
+import axios from "axios";
+
+// assets
+import { IconCircleCheck, IconCircleX } from "@tabler/icons";
 
 // ==============================|| Create/Update Customer View ||============================== //
 
@@ -23,7 +34,9 @@ let custPOCValues = {
   office_phone: "",
   title: "",
 };
+
 let custFilledPOCValues = {};
+
 const custRowsTransform = (custRows) => {
   if (!custRows.hasOwnProperty("name")) {
     custRows.name = "";
@@ -70,6 +83,7 @@ const custNewOrEdit = (dataEntryType) => {
 
 const CustDataEntryPage = () => {
   const { state } = useLocation();
+  let navigate = useNavigate();
   let custValues = custRowsTransform(state.row);
   let [mainCardTitle, hasContactBool] = custNewOrEdit(state.dataEntryType);
 
@@ -77,15 +91,97 @@ const CustDataEntryPage = () => {
   const [custData, setCustData] = React.useState(custValues);
   const [contactUpdate, setContactUpdate] = React.useState(false);
   const [hasSubmitted, setHasSubmitted] = React.useState(false);
+  const [getError, setError] = React.useState([false, ""]);
+  const baseURL = "http://localhost:8080/li-pca/v1/customers";
 
-  if (hasSubmitted) {
-    // TODO: Add Axios functions to POST or PUT data
-    console.log(custData);
-  }
+  React.useEffect(() => {
+    if (hasSubmitted && mainCardTitle == "New Customer") {
+      axios
+        .post(baseURL, custData, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setError([false, ""]);
+        })
+        .catch((error) => {
+          setError([true, error.message]);
+          console.log("Error fetching data");
+          console.log(error);
+        });
+    }
+  }, [custData, hasSubmitted, mainCardTitle]);
+  const closeDialog = () => {
+    setHasSubmitted(false);
+    if (!getError[0]) {
+      navigate("/li-pca-app/customers");
+    }
+  };
+
   return (
     <MainCard title={mainCardTitle}>
       <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1000 }}>
         <Grid container spacing={2}>
+          {getError[0] ? (
+            <Dialog open={hasSubmitted}>
+              <Grid sx={{ textAlign: "center" }}>
+                <DialogTitle>
+                  <Grid item>
+                    <IconCircleX color="#E62C22" size={100} stroke={1} />
+                  </Grid>
+                  <Grid item>{getError[1]}</Grid>
+                </DialogTitle>
+                <DialogContent>
+                  <Grid item>
+                    <DialogContentText>
+                      {mainCardTitle} changes are unable to be saved.
+                      <br />
+                      Check the console log for more details.
+                    </DialogContentText>
+                  </Grid>
+                  <Grid item sx={{ mt: 3, mb: 1 }}>
+                    <Button
+                      onClick={closeDialog}
+                      size="large"
+                      variant="contained"
+                    >
+                      Ok
+                    </Button>
+                  </Grid>
+                </DialogContent>
+              </Grid>
+            </Dialog>
+          ) : (
+            <Dialog open={hasSubmitted}>
+              <Grid sx={{ textAlign: "center" }}>
+                <DialogTitle>
+                  <Grid item>
+                    <IconCircleCheck color="#00b341" size={100} stroke={1} />
+                  </Grid>
+                  <Grid item>Successful save</Grid>
+                </DialogTitle>
+                <DialogContent>
+                  <Grid item>
+                    <DialogContentText>
+                      {mainCardTitle} changes have been made successfully.
+                    </DialogContentText>
+                  </Grid>
+                  <Grid item sx={{ mt: 3, mb: 1 }}>
+                    <Button
+                      onClick={closeDialog}
+                      size="large"
+                      variant="contained"
+                    >
+                      Ok
+                    </Button>
+                  </Grid>
+                </DialogContent>
+              </Grid>
+            </Dialog>
+          )}
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <Typography variant="h4" gutterBottom component="div">
               Customer Information
