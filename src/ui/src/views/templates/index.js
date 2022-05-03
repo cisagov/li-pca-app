4f927c3e56e10b0d2868ff1757bc0a413536e77c
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-05-03 19:38:41 -0400

    Update templates/index.js to use dataentry.js and axios

--- src/ui/src/views/templates/index.js
+++ src/ui/src/views/templates/index.js
@@ -1,93 +1,121 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
 // material-ui
-import axios from "axios";
 import { Grid } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Grid";
 
 // project imports
 import MainCard from "ui-component/cards/MainCard";
 import MainDataTable from "ui-component/tables/MainDataTable";
 
-// ==============================|| SAMPLE PAGE ||============================== //
+// third party
+import axios from "axios";
 
-// const rows2 = () => {
-//   axios.get( "http://localhost:8080/li-pca/v1/templates").then(res => {
-//       const templates = res.data.map((data) => ({
-//         ...data,
-//         id: data.uuid
-//       }));
-//       console.log(JSON.stringify(templates));
-//       return templates
-//   });
-// }
+// ==============================|| Templates view ||============================== //
 
-const rows = [
-  {
-    id: "d82c687d-46b0-49f6-b640-dc574082b970",
-    created: "2022-04-07T15:55:01.081000+00:00",
-    created_by: "bot",
-    deception_score: 1,
-    from_address: "1",
-    name: "Delayed Order",
-    retired: false,
-    retired_description: "",
-    uuid: "3913708c-4c0c-437a-820f-2898833ac6e8",
-  },
-  {
-    id: "7bb2c80b-66ff-4088-9a9a-7c9d24286b7c",
-    created: "2022-04-07T15:59:25.740000+00:00",
-    created_by: "bot",
-    deception_score: 1,
-    from_address: "test@test.com",
-    name: "Order Processing Issue",
-    retired: false,
-    retired_description: "",
-    uuid: "006e2558-4d77-4b22-88ee-5fe32365aaaa",
-  },
-  {
-    id: "b32023f6-bd59-4b1f-a2c0-7badf6830ef3",
-    created: "2022-04-07T15:59:49.916000+00:00",
-    created_by: "bot",
-    deception_score: 1,
-    from_address: "test@test.com",
-    name: "Shipping Details",
-    retired: false,
-    retired_description: "",
-    uuid: "95f02030-67d0-4a5a-9bb1-a6696a847b53",
-  },
-];
-
-const columns = [
-  { field: "id", hide: true },
-  { field: "name", headerName: "Template Name", flex: 4 },
-  { field: "deception_score", headerName: "Deception Score", flex: 1 },
-  { field: "created_by", headerName: "Created By", flex: 2 },
-  {
-    field: "col4",
-    headerName: "Edit",
-    sortable: false,
-    disableClickEventBubbling: true,
-    renderCell: () => {
-      return (
-        <IconButton variant="contained" color="primary">
-          <EditIcon />
-        </IconButton>
-      );
-    },
-    flex: 0.5,
-  },
-];
-const data = { rows: rows, columns: columns };
-const TemplatePage = () => (
-  <MainCard title="Templates">
-    <Grid container spacing={2}>
-      <Grid item xs={8}>
-        Template data shown below.
+function BaseJSX(props) {
+  const cols = [
+    { field: "id", hide: true },
+    { field: "name", headerName: "Template Name", flex: 4 },
+    { field: "deception_score", headerName: "Deception Score", flex: 1 },
+    { field: "created_by", headerName: "Created By", flex: 2 },
+  ];
+  return (
+    <MainCard title="Templates">
+      <Grid container spacing={2}>
+        <Grid item xs={8} sm={12} md={12} lg={12} xl={12}>
+          {props.children}
+          <MainDataTable
+            data={{ rows: props.rows, columns: cols }}
+            newEntryRoute={props.dataEntry}
+            editEntryRoute={props.dataEntry}
+          />
+        </Grid>
       </Grid>
-    </Grid>
-    <br />
-    <MainDataTable data={data} newEntryRoute="newTemplate" />
-  </MainCard>
-);
+    </MainCard>
+  );
+}
 
-export default TemplatePage;
+BaseJSX.propTypes = {
+  rows: PropTypes.array,
+  children: PropTypes.object,
+  dataEntry: PropTypes.string,
+};
+
+function TemplatesPage() {
+  const baseURL = "http://localhost:8080/li-pca/v1/templates";
+  const [isLoading, setLoading] = useState(true);
+  const [getData, setData] = useState([]);
+  const [getError, setError] = useState([false, ""]);
+
+  const temRows = (rowsArray) => {
+    if (Object.keys(rowsArray).length !== 0) {
+      let counter = 0;
+      let temRows = [];
+      temRows = Array.from(rowsArray);
+      temRows.forEach((entry) => {
+        entry["id"] = counter;
+        counter = counter + 1;
+      });
+      return rowsArray;
+    }
+    return [];
+  };
+  useEffect(() => {
+    axios
+      .get(baseURL, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+        setError([false, ""]);
+      })
+      .catch((error) => {
+        setError([true, error.message]);
+        setLoading(false);
+        console.log(error);
+      });
+  }, []);
+
+  //  Mock data test
+  // const jsonRows = require("./mockTemData.json");
+  // const rows = temRows(jsonRows);
+  const rows = temRows(getData);
+
+  if (isLoading) {
+    return (
+      <BaseJSX rows={[]} dataEntry={""}>
+        <Typography>Loading...</Typography>
+      </BaseJSX>
+    );
+  } else if (getError[0]) {
+    return (
+      <BaseJSX rows={[]} dataEntry={""}>
+        <Alert severity="error" sx={{ mb: 2 }}>
+          {getError[1]}. Unable to load customer data from the database.
+        </Alert>
+      </BaseJSX>
+    );
+  } else if (rows.length === 0) {
+    return (
+      <BaseJSX rows={[]} dataEntry={"data-entry"}>
+        <Typography sx={{ mb: 2 }}>No customer data entries found.</Typography>
+      </BaseJSX>
+    );
+  }
+  return (
+    <BaseJSX rows={rows} dataEntry={"data-entry"}>
+      <Typography sx={{ mb: 2 }}>
+        Customer data from the database shown below.
+      </Typography>
+    </BaseJSX>
+  );
+}
+export default TemplatesPage;
