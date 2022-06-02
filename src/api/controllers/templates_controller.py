d0e9cfb9d4d01ce30d522034f0a313a06942e967
nickviola <nick.viola@associates.cisa.dhs.gov>
2022-06-02 12:01:40 -0500

    Add Templates API Endpoint for bulk updates

--- src/api/controllers/templates_controller.py
+++ src/api/controllers/templates_controller.py
@@ -14,6 +14,51 @@ from api.db_manager import TemplateManager
 db_manager = TemplateManager()
 
 
+def handle_multiple_target_updates(body):  # noqa: E501
+    """Take array of template objects to bulk update in DB.
+
+     # noqa: E501
+
+    :type list: dict
+
+    :rtype: list: dict
+    """
+    logging.debug("Body: %s", body)
+    updated_templates = []
+
+    for item in body:
+        logging.debug("Template Update Data: %s", item)
+
+        # Sanitize PUT data
+        target_uuid = item.pop("uuid", None)
+        target_id = item.pop("_id", None)
+        created = item.pop("created", None)
+        created_by = item.pop("created_by", None)
+        updated = item.pop("created", None)
+        updated_by = item.pop("created_by", None)
+
+        # Log Sanitized Values
+        logging.debug(
+            "Stripping values: %s",
+            [
+                target_id,
+                target_uuid,
+                created,
+                created_by,
+                updated,
+                updated_by,
+            ],
+        )
+
+        # Update each object in DB and append to response array.
+        if target_id:
+            result = db_manager.update(document_id=target_id, data=item)
+            updated_templates.append(result)
+
+    # Return List of updated templates
+    return updated_templates
+
+
 def create_template(body=None):  # noqa: E501
     """Add a new template to the data store.
 
@@ -89,3 +134,14 @@ def update_template_by_uuid(uuid, body=None):  # noqa: E501
     """
     logging.debug("uuid: %s", uuid)
     return db_manager.update(document_id=uuid, data=connexion.request.get_json())
+
+
+def bulk_update_templates():  # noga: E501
+    """Update an existing template.
+
+     # noqa: E501
+
+    :rtype: list: dict
+    """
+    body = connexion.request.get_json()
+    return handle_multiple_target_updates(body)
