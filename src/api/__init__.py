02fbf33b90224ad82dfab07e47da4bfdf59416c1
Nick Viola <6486652+nickviola@users.noreply.github.com>
2022-07-26 12:49:02 -0500

    Update src/api/__init__.py comments and import ordering to be alphabetical
    Co-authored-by: dav3r <david.redmin@trio.dhs.gov>

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,12 +1,11 @@
 """The Li-PCA API library."""
+# cisagov Libraries
 # We disable a Flake8 check for "Module imported but unused (F401)" because
 # although this import is not directly used, it populates the value
 # package_name.__version__, which is used to get version information about this
 # Python package.
-# cisagov Libraries
-from api.controllers import customers_controller
-
 from ._version import __version__  # noqa: F401
+from api.controllers import customers_controller
 
 __all__ = [
     "customers_controller",
