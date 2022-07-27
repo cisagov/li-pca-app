e531ebd18cf6a7627f559abef0d4c3092326ee3c
nickviola <nick.viola@associates.cisa.dhs.gov>
2022-07-27 12:55:31 -0500

    Fix precommit failures conflicting failure about space after/before class description. ignored D205

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,8 +4,10 @@
 # although this import is not directly used, it populates the value
 # package_name.__version__, which is used to get version information about this
 # Python package.
-from ._version import __version__  # noqa: F401
+# cisagov Libraries
 from api.controllers import customers_controller
+
+from ._version import __version__  # noqa: F401
 
 __all__ = [
     "customers_controller",
