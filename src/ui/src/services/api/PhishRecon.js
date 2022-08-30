eca0142506db9de4c5960627f1277c03f68cc12f
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-08-29 20:59:31 -0400

    Update harvester axios call to a standard function callback

--- src/ui/src/services/api/PhishRecon.js
+++ src/ui/src/services/api/PhishRecon.js
@@ -37,55 +37,45 @@ export const useGetAll = (fetchData) => {
   };
 };
 
-export const useGetHarvesterResults = (
-  domain,
-  fullCustEntry,
-  triggerDataFetch
-) => {
-  const [isLoading, setLoading] = useState(true);
-  const [getHarvesterData, setHarvesterData] = useState([]);
-  const [getError, setError] = useState([false, ""]);
-
-  useEffect(async () => {
-    if (domain) {
-      try {
-        let response = await axios.get(harvesterBaseUrl + "?domain=" + domain, {
-          headers: headers,
-        });
-        let partialCustEntry = {
-          recon_results: [],
-        };
-        let reconResults = [];
-        if (fullCustEntry.hasOwnProperty("recon_results")) {
-          reconResults.push(...fullCustEntry.recon_results);
-        }
-        const currentTime = new Date();
-        response.data.recon_time = currentTime.toISOString();
-        response.data.domain = domain;
-        reconResults.push(response.data);
-        partialCustEntry["recon_results"] = reconResults;
-        let resp = await axios.put(
-          custBaseURL + "/" + fullCustEntry._id,
-          partialCustEntry,
-          { headers: headers }
-        );
-        console.log("Response from the Harvester", response);
-        console.log("Response from saving to the database", resp);
-        setHarvesterData(response.data);
-        triggerDataFetch();
-        setLoading(false);
-        setError([false, ""]);
-      } catch (error) {
-        setError([true, error.message]);
-        setLoading(false);
-        console.log(error);
-      }
+export async function fetchData(
+  selectedRow,
+  triggerDataFetch,
+  setLoading,
+  setHarvesterData,
+  setError
+) {
+  try {
+    let response = await axios.get(
+      harvesterBaseUrl + "?domain=" + selectedRow.domain,
+      { headers: headers }
+    );
+    let partialCustEntry = {
+      recon_results: [],
+    };
+    let reconResults = [];
+    if (selectedRow.hasOwnProperty("recon_results")) {
+      reconResults.push(...selectedRow.recon_results);
     }
-  }, [domain]);
-  return {
-    isLoading,
-    getData: getHarvesterData,
-    getError,
-    domain,
-  };
-};
+    const currentTime = new Date();
+    console.log(response.data);
+    response.data.recon_time = currentTime.toISOString();
+    response.data.domain = selectedRow.domain;
+    reconResults.push(response.data);
+    partialCustEntry["recon_results"] = reconResults;
+    let resp = await axios.put(
+      custBaseURL + "/" + selectedRow._id,
+      partialCustEntry,
+      { headers: headers }
+    );
+    console.log("Response from the Harvester", response);
+    console.log("Response from saving to the database", resp);
+    setHarvesterData(response.data);
+    triggerDataFetch();
+    setLoading(false);
+    setError([false, ""]);
+  } catch (error) {
+    setError([true, error.message]);
+    setLoading(false);
+    console.log(error);
+  }
+}
