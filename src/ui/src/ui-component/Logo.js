ada30de87750d3c381f53e478af72bf6dd72ce8b
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-10-14 15:41:28 -0400

    Update logo from li-pca to cat-phishing

--- src/ui/src/ui-component/Logo.js
+++ src/ui/src/ui-component/Logo.js
@@ -1,13 +1,13 @@
 // material-ui
 // import { useTheme } from '@mui/material/styles';
 
-import logo from "assets/images/lipca_logo.png";
+import logo from "assets/images/cat-phishing.png";
 
 // ==============================|| LOGO SVG ||============================== //
 
 const Logo = () => {
   // const theme = useTheme();
-  return <img src={logo} alt="Berry" width="100" />;
+  return <img src={logo} alt="Berry" width="180" />;
 };
 
 export default Logo;
