8f865a22a59ab20c881a13f60a02f95570769645
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-11-16 17:52:21 -0500

    Integrate TemplateAttributeForm with main dataentry file

--- src/ui/src/ui-component/forms/TemplateAttributeForm.js
+++ src/ui/src/ui-component/forms/TemplateAttributeForm.js
@@ -1,19 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 //material-ui
 import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-
-//third party
-import { useFormik } from "formik";
-import * as yup from "yup";
 
 // project imports
 import TemplateMultiSelectChip from "../user-input/TemplateMultiSelectChip";
@@ -21,33 +16,20 @@ import TemplateAttrRecsForm from "./TemplateAttrRecsForm";
 import { useGetAll as domainGetAll } from "services/api/SendingDomains";
 import { useGetAll as landingPgGetAll } from "services/api/LandingPages";
 
-const sophJson = require("views/templates/sophisticatedTags.json");
-const redFlagJson = require("views/templates/redFlagTags.json");
+const recsJson = require("views/templates/recommendations.json");
+const sophJson = recsJson.filter(
+  (entry) => entry.recommendation_type === "Sophisticated"
+);
+const redFlagJson = recsJson.filter(
+  (entry) => entry.recommendation_type === "Red Flag"
+);
 
 const TemplateAttrForm = (props) => {
   const [sophisticatedArray, setSophArray] = useState(sophJson);
   const [redFlagArray, setRFArray] = useState(redFlagJson);
-  const [selectedSophTags, setSophTags] = useState([]);
-  const [selectedRFTags, setRFTags] = useState([]);
   let domain = domainGetAll();
   let landingPage = landingPgGetAll();
 
-  const validationSchema = yup.object({
-    subject: yup.string().required("Subject is required"),
-    name: yup.string().required("Template Name is required"),
-    from_address: yup.string().required("Sender is required"),
-  });
-  const formik = useFormik({
-    initialValues: props.initialTemplateValues,
-    validationSchema: validationSchema,
-    validateOnChange: true,
-    onSubmit: (values) => {
-      values["sophisticated"] = selectedSophTags;
-      values["red_flag"] = selectedRFTags;
-      props.setTemplateData(Object.assign(props.templateData, values));
-      props.setHasSubmitted(true);
-    },
-  });
   const message = (str) => {
     let msg =
       "Either no " +
@@ -61,26 +43,32 @@ const TemplateAttrForm = (props) => {
       </Tooltip>
     );
   };
+
   return (
     <Box sx={{ midWidth: 300, maxWidth: 1000 }}>
-      <form id="template-attr-form" onSubmit={formik.handleSubmit}>
+      <form id="template-form" onSubmit={props.formik.handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <Typography variant="h4" gutterBottom component="div">
               Subject:
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={12} md={11} lg={10} xl={9}>
+          <Grid item xs={12} sm={12} md={12} lg={11} xl={10}>
             <TextField
               size="small"
               fullWidth
               id="subject"
               name="subject"
               label="Subject *"
-              value={formik.values.subject}
-              onChange={formik.handleChange}
-              error={formik.touched.subject && Boolean(formik.errors.subject)}
-              helperText={formik.touched.subject && formik.errors.subject}
+              value={props.formik.values.subject}
+              onChange={props.formik.handleChange}
+              error={
+                props.formik.touched.subject &&
+                Boolean(props.formik.errors.subject)
+              }
+              helperText={
+                props.formik.touched.subject && props.formik.errors.subject
+              }
             />
           </Grid>
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
@@ -88,17 +76,19 @@ const TemplateAttrForm = (props) => {
               From Address:
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={6} md={5} lg={5} xl={4}>
+          <Grid item xs={12} sm={6} md={6} lg={6} xl={5}>
             <TextField
               size="small"
               fullWidth
-              id="name"
-              name="name"
+              id="text"
+              name="text"
               label="Display Name *"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
+              value={props.formik.values.text}
+              onChange={props.formik.handleChange}
+              error={
+                props.formik.touched.text && Boolean(props.formik.errors.text)
+              }
+              helperText={props.formik.touched.text && props.formik.errors.text}
             />
           </Grid>
           <Grid item xs={12} sm={6} md={6} lg={5} xl={5}>
@@ -108,39 +98,40 @@ const TemplateAttrForm = (props) => {
               id="from_address"
               name="from_address"
               label="Sender *"
-              value={formik.values.from_address}
-              onChange={formik.handleChange}
+              value={props.formik.values.from_address}
+              onChange={props.formik.handleChange}
               error={
-                formik.touched.from_address &&
-                Boolean(formik.errors.from_address)
+                props.formik.touched.from_address &&
+                Boolean(props.formik.errors.from_address)
               }
               helperText={
-                formik.touched.from_address && formik.errors.from_address
+                props.formik.touched.from_address &&
+                props.formik.errors.from_address
               }
             />
           </Grid>
-          <Grid item xs={12} xl={11}>
-            {formik.values.name} &#60;{formik.values.from_address}@domain.com
-            &#62;
+          <Grid item s={12} sm={12} md={12} lg={12} xl={12}>
+            {props.formik.values.text} &#60;{props.formik.values.from_address}
+            @domain.com&#62;
           </Grid>
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <Typography variant="h4" gutterBottom component="div">
               Recommendations:
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={12} md={11} lg={10} xl={9}>
+          <Grid item xs={12} sm={12} md={12} lg={11} xl={10}>
             <TemplateMultiSelectChip
               values={sophisticatedArray}
-              selection={selectedSophTags}
-              setSelection={setSophTags}
+              selection={props.selectedSophTags}
+              setSelection={props.setSophTags}
               label="Sophisticated"
             />
           </Grid>
-          <Grid item xs={12} sm={12} md={11} lg={10} xl={9}>
+          <Grid item xs={12} sm={12} md={12} lg={11} xl={10}>
             <TemplateMultiSelectChip
               values={redFlagArray}
-              selection={selectedRFTags}
-              setSelection={setRFTags}
+              selection={props.selectedRFTags}
+              setSelection={props.setRFTags}
               label="Red Flag"
             />
           </Grid>
@@ -152,14 +143,14 @@ const TemplateAttrForm = (props) => {
         setSophArray={setSophArray}
         setRFArray={setRFArray}
       />
-      <form id="template-attr-form" onSubmit={formik.handleSubmit}>
+      <form id="template-form" onSubmit={props.formik.handleSubmit}>
         <Grid container spacing={2} sx={{ mt: 2 }}>
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <Typography variant="h4" gutterBottom component="div">
-              Subscription Config:
+              Campaign Config:
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={12} md={11} lg={10} xl={9}>
+          <Grid item xs={12} sm={12} md={12} lg={11} xl={10}>
             {domain.getError[0] || domain.getData.length == 0 ? (
               message("sending domains")
             ) : (
@@ -170,15 +161,15 @@ const TemplateAttrForm = (props) => {
                 label="Sending Domain Selection"
                 id="sending_profile_id"
                 name="sending_profile_id"
-                value={formik.values.sending_profile_id}
-                onChange={formik.handleChange}
+                value={props.formik.values.sending_profile_id}
+                onChange={props.formik.handleChange}
                 error={
-                  formik.touched.sending_profile_id &&
-                  Boolean(formik.errors.sending_profile_id)
+                  props.formik.touched.sending_profile_id &&
+                  Boolean(props.formik.errors.sending_profile_id)
                 }
                 helperText={
-                  formik.touched.sending_profile_id &&
-                  formik.errors.sending_profile_id
+                  props.formik.touched.sending_profile_id &&
+                  props.formik.errors.sending_profile_id
                 }
               >
                 {domain.getData.map((entry) => {
@@ -192,7 +183,7 @@ const TemplateAttrForm = (props) => {
               </TextField>
             )}
           </Grid>
-          <Grid item xs={12} sm={12} md={11} lg={10} xl={9}>
+          <Grid item xs={12} sm={12} md={12} lg={11} xl={10}>
             {landingPage.getError[0] || landingPage.getData.length == 0 ? (
               message("landing pages")
             ) : (
@@ -203,15 +194,15 @@ const TemplateAttrForm = (props) => {
                 label="Landing Page Selection"
                 id="landing_page_id"
                 name="landing_page_id"
-                value={formik.values.landing_page_id}
-                onChange={formik.handleChange}
+                value={props.formik.values.landing_page_id}
+                onChange={props.formik.handleChange}
                 error={
-                  formik.touched.landing_page_id &&
-                  Boolean(formik.errors.landing_page_id)
+                  props.formik.touched.landing_page_id &&
+                  Boolean(props.formik.errors.landing_page_id)
                 }
                 helperText={
-                  formik.touched.landing_page_id &&
-                  formik.errors.landing_page_id
+                  props.formik.touched.landing_page_id &&
+                  props.formik.errors.landing_page_id
                 }
               >
                 {landingPage.getData.map((entry) => {
@@ -225,18 +216,6 @@ const TemplateAttrForm = (props) => {
               </TextField>
             )}
           </Grid>
-          <Grid item xs={12} sm={5} md={5} lg={5} xl={5}>
-            <Button
-              fullWidth
-              color="info"
-              variant="contained"
-              size="large"
-              disabled={!formik.dirty}
-              type="submit"
-            >
-              Save Template
-            </Button>
-          </Grid>
           <Grid item xs={10} sm={1} md={1} lg={1} xl={1} />
         </Grid>
       </form>
@@ -245,13 +224,11 @@ const TemplateAttrForm = (props) => {
 };
 
 TemplateAttrForm.propTypes = {
-  initialTemplateValues: PropTypes.object,
-  setTemplateData: PropTypes.func,
-  templateData: PropTypes.object,
-  setHasSubmitted: PropTypes.func,
-  dataEntryType: PropTypes.string,
-  setDelete: PropTypes.func,
-  identifiers: PropTypes.array,
+  formik: PropTypes.object,
+  setSophTags: PropTypes.func,
+  setRFTags: PropTypes.func,
+  selectedRFTags: PropTypes.array,
+  selectedSophTags: PropTypes.array,
 };
 
 export default TemplateAttrForm;
