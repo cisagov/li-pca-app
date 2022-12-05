0dd3fe98d6124a1c4f4978863f24d577ede3533a
ameliav <amelia.vance@associates.cisa.dhs.gov>
2022-12-05 10:54:19 -0500

    Update campaigns/dataentry.js to break up stepper steps

--- src/ui/src/views/campaigns/dataentry.js
+++ src/ui/src/views/campaigns/dataentry.js
@@ -1,28 +1,26 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
 
 // material-ui
-import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
+import StepButton from "@mui/material/StepButton";
+import Stepper from "@mui/material/Stepper";
 import Typography from "@mui/material/Typography";
 
 // project imports
-// import CampaignInitialForm from "ui-component/forms/CampaignInitialForm";
+import CampaignInitialForm from "ui-component/forms/CampaignInitialForm";
 // import CampaignTemplateForm from "ui-component/forms/CampaignTemplateForm";
 import MainCard from "ui-component/cards/MainCard";
 
 // ==============================|| Create/Update Campaign View ||============================== //
 
 const steps = [
-  "Campaign Assignment",
-  "Phishing Material",
-  "Delivery Assignment",
-  "Review/Send",
+  "Initial Setup & Target Selection",
+  "Select Phishing Template",
+  "Set Delivery Schedule",
+  "Review & Send",
 ];
 
 const CampaignDataEntryPage = () => {
@@ -32,51 +30,61 @@ const CampaignDataEntryPage = () => {
   const isStepSkipped = (step) => {
     return skipped.has(step);
   };
-
   const handleNext = () => {
     let newSkipped = skipped;
     if (isStepSkipped(activeStep)) {
       newSkipped = new Set(newSkipped.values());
       newSkipped.delete(activeStep);
     }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
     setSkipped(newSkipped);
   };
-
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
-  const handleSkip = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
   const handleReset = () => {
     setActiveStep(0);
   };
-
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+  const stepButtons = (
+    <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+      <Button
+        color="inherit"
+        disabled={activeStep === 0}
+        onClick={handleBack}
+        sx={{ mr: 1 }}
+      >
+        Back
+      </Button>
+      <Box sx={{ flex: "1 1 auto" }} />
+      <Button onClick={handleNext}>
+        {activeStep === steps.length - 1 ? "Finish" : "Next"}
+      </Button>
+    </Box>
+  );
   return (
     <MainCard title={"New Campaign" + " Wizard"}>
-      <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1000 }}>
+      <Box sx={{ ml: 5, mr: 5, mt: 3, maxWidth: 1300 }}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <Box sx={{ width: "100%" }}>
-              <Stepper activeStep={activeStep}>
+              <Stepper
+                activeStep={activeStep}
+                sx={{ mb: 5, fontWeight: "bold" }}
+                alternativeLabel
+              >
                 {steps.map((label, index) => {
                   const stepProps = {};
-                  const labelProps = {};
                   if (isStepSkipped(index)) {
                     stepProps.completed = false;
                   }
                   return (
                     <Step key={label} {...stepProps}>
-                      <StepLabel {...labelProps}>{label}</StepLabel>
+                      <StepButton color="inherit" onClick={handleStep(index)}>
+                        {label}
+                      </StepButton>
                     </Step>
                   );
                 })}
@@ -91,25 +99,29 @@ const CampaignDataEntryPage = () => {
                     <Button onClick={handleReset}>Reset</Button>
                   </Box>
                 </>
+              ) : activeStep == 0 ? (
+                <>
+                  <CampaignInitialForm />
+                  {stepButtons}
+                </>
+              ) : activeStep == 1 ? (
+                <>
+                  {/* <CampaignTemplateForm /> */}
+                  {stepButtons}
+                </>
+              ) : activeStep == 2 ? (
+                <>
+                  <Typography sx={{ mt: 2, mb: 1 }}>
+                    Step {activeStep + 1}
+                  </Typography>
+                  {stepButtons}
+                </>
               ) : (
                 <>
                   <Typography sx={{ mt: 2, mb: 1 }}>
                     Step {activeStep + 1}
                   </Typography>
-                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                    <Button
-                      color="inherit"
-                      disabled={activeStep === 0}
-                      onClick={handleBack}
-                      sx={{ mr: 1 }}
-                    >
-                      Back
-                    </Button>
-                    <Box sx={{ flex: "1 1 auto" }} />
-                    <Button onClick={handleNext}>
-                      {activeStep === steps.length - 1 ? "Finish" : "Next"}
-                    </Button>
-                  </Box>
+                  {stepButtons}
                 </>
               )}
             </Box>
