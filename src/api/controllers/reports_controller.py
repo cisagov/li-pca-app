e2c59a91ff1bd251f56be00906052e7e8bd7bcb7
ameliav <amelia.vance@associates.cisa.dhs.gov>
2023-02-24 14:23:08 -0500

    Add new file reports_controller.py

---
+++ src/api/controllers/reports_controller.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""Report Controller Logic."""
+
+# Standard Python Libraries
+import logging
+
+# Third-Party Libraries
+import connexion
+from flask import jsonify
+
+# cisagov Libraries
+from api.db_manager import ReportManager
+
+db_manager = ReportManager()
+
+
+def create_report(body=None):  # noqa: E501
+    """Add a new report to the data store.
+
+     # noqa: E501
+
+    :param body: Report object to be added to data store
+    :type body: dict | bytes
+
+    :rtype: None
+    """
+    logging.debug("Conn request: %s", connexion.request.get_json())
+    if connexion.request.is_json:
+        body = connexion.request.get_json()
+        logging.debug("Body: %s", body)
+        return jsonify(db_manager.save(body))
+
+
+def delete_report_by_uuid(uuid):  # noqa: E501
+    """Delete a Report.
+
+     # noqa: E501
+
+    :param uuid: uuid to delete
+    :type uuid: str
+
+    :rtype: None
+    """
+    logging.debug("uuid: %s", uuid)
+    return jsonify(db_manager.delete(uuid))
+
+
+def get_all_reports(filter_params=None):  # noqa: E501
+    """Find all Reports.
+
+    Multiple status values can be provided with comma separated strings  # noqa: E501
+
+    :param name: template name filter
+    :type name: List[str]
+
+    :rtype: List[Template]
+    """
+    logging.debug("request args: %s", connexion.request.args)
+    filter_params = connexion.request.args
+    return jsonify(db_manager.all(params=db_manager.get_query(filter_params)))
+
+
+def get_report_by_uuid(uuid):  # noqa: E501
+    """Find template by uuid.
+
+    Returns a single Report # noqa: E501
+
+    :param uuid: uuid of template to return
+    :type uuid: str
+
+    :rtype: Template
+    """
+    logging.debug("uuid: %s", uuid)
+    return jsonify(db_manager.get(document_id=uuid))
+
+
+def update_report_by_uuid(uuid, body=None):  # noqa: E501
+    """Update an existing Report.
+
+     # noqa: E501
+
+    :param uuid: uuid of Report to update
+    :type uuid: str
+    :param body: Report object to be added to data store
+    :type body: dict | bytes
+
+    :rtype: None
+    """
+    logging.debug("uuid: %s", uuid)
+    return db_manager.update(document_id=uuid, data=connexion.request.get_json())
